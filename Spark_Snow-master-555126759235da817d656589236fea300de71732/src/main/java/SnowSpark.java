b967ed57117b10e7c8156683752db7bcbc8368c0
Justin Snow <jrsnow8921@gmail.com>
2015-11-18 09:17:42 -0500

    Intial build

---
+++ Spark_Snow-master-555126759235da817d656589236fea300de71732/src/main/java/SnowSpark.java
@@ -0,0 +1,23 @@
+/* SnowSpark.java */
+import org.apache.spark.api.java.*;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.function.Function;
+
+public class SnowSpark {
+  public static void main(String[] args) {
+    String logFile = "C:\\Users\\jsnow\\Desktop\\Spark_project\\Log.md"; // Should be some file on your system
+    SparkConf conf = new SparkConf().setAppName("Snow Spark");
+    JavaSparkContext sc = new JavaSparkContext(conf);
+    JavaRDD<String> logData = sc.textFile(logFile).cache();
+
+    long numAs = logData.filter(new Function<String, Boolean>() {
+      public Boolean call(String s) { return s.contains("a"); }
+    }).count();
+
+    long numBs = logData.filter(new Function<String, Boolean>() {
+      public Boolean call(String s) { return s.contains("b"); }
+    }).count();
+
+    System.out.println("Lines with a: " + numAs + ", lines with b: " + numBs);
+  }
+}
